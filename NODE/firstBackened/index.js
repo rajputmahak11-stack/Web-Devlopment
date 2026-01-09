84ecc7358657e64d88a9db748085d20e57e90ddf
mahakrajput16 <rajputmahak11@gmail.com>
2026-01-09 21:09:13 +0530

    mongoDB1

--- NODE/firstBackened/index.js
+++ NODE/firstBackened/index.js
@@ -1,8 +1,18 @@
 import dotenv from'dotenv'
 dotenv.config();
 import express, { response } from 'express'
+import connectDB from './src/config/db.js';
+import Authrouter from "./src/routers/myRouter.js"
 
 const app =express();
+app.use(express.json());
+
+app.use("/auth",Authrouter);
+
+
+
+
+
 
 app.get("/",(req,res)=>{
     console.log("Server is running");
@@ -12,5 +22,6 @@ app.get("/",(req,res)=>{
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log("server started port",port);
+    connectDB();
 });
 
